e291ea682816897b6aec9c081e07aaf824f83981
Mindaugas Bujanauskas <bujanauskas.m@gmail.com>
2017-08-07 08:49:25 +0300

    Initial commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import SearchPage from './components/search-page';
+
+ReactDom.render(
+    <SearchPage />,
+    document.getElementById('root')
+);
